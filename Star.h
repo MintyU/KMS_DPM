84ad10fd49c90eae8f1855bb34e48e397c57fdd0
MintyU <deathcat@outlook.kr>
2020-03-22 14:45:02 +0900

    #add gitignore & change logic

--- Star.h
+++ Star.h
@@ -1,6 +1,8 @@
 #ifndef STAR_H
 #define STAR_H
 
+#include <iostream>
+using namespace std;
 class Star
 {
 private:
@@ -8,12 +10,23 @@ private:
     int Target;
     int MVP;
     bool Starcatch;
+
 public:
     void InitReqLev();
     void InitTarget();
-    void InitTarget();
+    void InitMVP();
     void DoStarCatch();
 };
 
+inline void Star::InitReqLev()
+{
+    cout << "Please enter Item Required Level(ex:100, 110, 120, 130, 140, 150, 160, 200) \n:";
+    cin >> ReqLev;
+    if (ReqLev < 100 || ReqLev > 200 || ReqLev % 10 != 0 || (ReqLev >= 161 && ReqLev <= 199))
+    {
+        cout << "It's not a Valid number. ";
+        Star::InitReqLev();
+    }
+}
 
 #endif
--- StarMain.cpp
+++ StarMain.cpp
@@ -5,5 +5,6 @@ using namespace std;
 
 int main()
 {
-
+    Star star;
+    star.InitReqLev();
 }
