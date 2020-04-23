58f78ed42528086404c990ad2aa348a6e4f12ca6
MintyU <deathcat@outlook.kr>
2020-04-23 20:58:49 +0900

    #inline func to cpp

--- Star.cpp
+++ Star.cpp
@@ -73,9 +73,151 @@ void Star::InitReqLev()
     }
 }
 
+
+void Star::InitTarget()
+{
+    cout << "How much would you like to enchant?";
+    switch (ReqLev)
+    {
+    case 100:
+        cout << "(100Lev item can be enchanted up to 8)\n: ";
+        cin >> Target;
+        if (Target > 8)
+        {
+            cout << "It's not a Valid number. ";
+            Star::InitTarget();
+        }
+        break;
+
+    case 110:
+        cout << "(110Lev item can be enchanted up to 10)\n: ";
+        cin >> Target;
+        if (Target > 10)
+        {
+            cout << "It's not a Valid number. ";
+            Star::InitTarget();
+        }
+        break;
+
+    case 120:
+        cout << "(120Lev item can be enchanted up to 15)\n: ";
+        cin >> Target;
+        if (Target > 15)
+        {
+            cout << "It's not a Valid number. ";
+            Star::InitTarget();
+        }
+        break;
+
+    case 130:
+        cout << "(130Lev item can be enchanted up to 20)\n: ";
+        cin >> Target;
+        if (Target > 20)
+        {
+            cout << "It's not a Valid number. ";
+            Star::InitTarget();
+        }
+        break;
+
+    case 140:
+        cout << "(140Lev item can be enchanted up to 25)\n: ";
+        cin >> Target;
+        if (Target > 25)
+        {
+            cout << "It's not a Valid number. ";
+            Star::InitTarget();
+        }
+        break;
+
+    case 150:
+        cout << "(150Lev item can be enchanted up to 25)\n: ";
+        cin >> Target;
+        if (Target > 25)
+        {
+            cout << "It's not a Valid number. ";
+            Star::InitTarget();
+        }
+        break;
+
+    case 160:
+        cout << "(160Lev item can be enchanted up to 25)\n: ";
+        cin >> Target;
+        if (Target > 25)
+        {
+            cout << "It's not a Valid number. ";
+            Star::InitTarget();
+        }
+        break;
+
+    case 200:
+        cout << "(200Lev item can be enchanted up to 25)\n: ";
+        cin >> Target;
+        if (Target > 25)
+        {
+            cout << "It's not a Valid number. ";
+            Star::InitTarget();
+        }
+        break;
+
+    default:
+        cout << "It's not a Valid number. ";
+        break;
+    }
+}
+
+
 void Star::StarForce()
 {
     int currentStar = 0;
     srand((unsigned int)time(NULL));
     cout << (rand() % 1000 / 10);
+}
+
+void Star::InitMVP()
+{
+    cout << "Please enter your MVP Rank\nNone, Bronze: 0\nSilver      : 1\nGold        : 2\nDiamond, Red: 3\n: ";
+    cin >> MVP;
+    if (MVP > 3)
+    {
+        cout << "It's not a Valid number. ";
+        Star::InitMVP();
+    }
+}
+
+void Star::DoStarCatch()
+{
+    cout << "You wanna do StarCatch?(probability 4.5%p rises)(y/n)\n: ";
+    cin >> ynstarcatch;
+    if (ynstarcatch == 'y' || ynstarcatch == 'Y')
+    {
+        Starcatch = true;
+        cout << "Starcatch True \n";
+    }
+    else if (ynstarcatch == 'n' || ynstarcatch == 'N')
+    {
+        Starcatch = false;
+        cout << "Starcatch False \n";
+    }
+    else
+    {
+        cout << "It's not a Valid number. ";
+        Star::DoStarCatch();
+    }
+}
+
+void Star::InitOIP()
+{
+    cout << "Please enter the original price of item(lowest price in oction)\n: ";
+    cin >> OriginalItemPrice;
+    Meso = OriginalItemPrice;
+}
+
+void Star::Valuecheck()
+{
+    cout << endl << ReqLev << endl;
+    cout << Target << endl;
+    cout << MVP << endl;
+    cout << ynstarcatch << endl;
+    cout << Starcatch << endl;
+
 }
--- StarMain.cpp
+++ StarMain.cpp
@@ -12,4 +12,5 @@ int main()
     star.DoStarCatch();
     star.InitOIP();
     star.StarForce();
+    star.Valuecheck();
 }
